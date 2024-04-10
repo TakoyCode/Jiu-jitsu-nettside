4a7d1ad1fc701547040e90a2e22594cfae433c8f
anneliny <157688768+anneliny@users.noreply.github.com>
2024-04-10 11:23:01 +0200

    Made mainView - belt

--- js/mainView.js
+++ js/mainView.js
@@ -4,8 +4,20 @@ function mainView() {
     html += /*HTML*/`
 
     ${createNavBar()}
-        <div>Test</div>
+    ${createBeltHtml()}
     
-    `;
-    app.innerHTML = html;
+`;
+app.innerHTML = html;
+}
+
+function createBeltHtml(){
+    return /*html*/`
+    <div class ="beltContainerMain">
+    <div class="beltBoxMain yellowBelt" onclick="">5.kyu - Gult belte</div>
+    <div class="beltBoxMain orangeBelt" onclick="">4.kyu - Oransje belte</div>
+    <div class="beltBoxMain greenBelt" onclick="">3.kyu - Grønt belte</div>
+    <div class="beltBoxMain blueBelt" onclick="">2.kyu - Blått belte</div>
+    <div class="beltBoxMain brownBelt" onclick="">1.kyu - Brunt belte</div>
+    </div>
+`;
 }
