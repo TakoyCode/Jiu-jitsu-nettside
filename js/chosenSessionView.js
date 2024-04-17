37c9ccf2c45aedffe3e9bf696e7380ab5fbacada
anneliny <157688768+anneliny@users.noreply.github.com>
2024-04-17 11:17:42 +0200

    chosenSessionView, model

--- js/chosenSessionView.js
+++ js/chosenSessionView.js
@@ -2,20 +2,32 @@
 function chosenSessionView(){
     const app = document.getElementById("app")
     let html = "";
-    html += /*HTML*/`
+    html = /*HTML*/`
     ${createNavBar()}
-    <h1 class="chosenSessionHeader">Økt</h1>
+     <h1 class="chosenSessionHeader">Overskrift</h1>
     <button class="chosenSessionBtn" onclick="">Slett</button>
     <hr style="width: 20%"/>
-
     <div class="chosenSessionContainer">
     <button class ="chosenSessionBtnBack" onclick="">Tilbake</button>
-    <div class="chosenSessionBox">Nivå:</div>
-    <div class="chosenSessionBox">En liten beskrivelse</div>
-    <div class="chosenSessionBox">Teknikknavn</div>
-    <div class="chosenSessionBox">VIDEO</div>
+    <div>${chosenSessionViewSessions()}</div>
     <button class="chosenSessionSlide"onclick=""><- 1 -></button>
 </div>
+
     `;
 app.innerHTML = html;
+}
+
+function chosenSessionViewSessions(){
+    let html = '';
+    const user = model.data.users[0];
+    for (let i = 0; i < user.sessions.length; i++){
+    const session = user.sessions[i];
+    html =/*html*/ `
+    <div class="chosenSessionBox">Nivå: ${session.level}</div>
+    <div class="chosenSessionBox">${session.description}</div>
+    <div class="chosenSessionBox">${session.name}</div>
+    <div class="chosenSessionBox">VIDEO</div>
+    `;
+    }
+    return html;
 }
